ec66d7a1bfb5698afdaea95c805a05b7c8f7c65c
Jordan Bluehdorn <Jordan@Jordans-MacBook-Pro.local>
2017-01-04 14:00:59 -0800

    GenerateColumnAnswer implemented as well. Gameboard init methods complete.

--- Unnamed Math Game/Assets/scripts/GameBoardController.cs
+++ Unnamed Math Game/Assets/scripts/GameBoardController.cs
@@ -21,7 +21,12 @@ public class GameBoardController : MonoBehaviour {
 		FirstRow = 0,
 		SecondRow = 1,
 		ThirdRow = 2
-	}
+	};
+	public enum column {
+		FirstColumn = 0,
+		SecondColumn = 1,
+		ThirdColumn = 2
+	};
 
 	private int[,] gameboardNums;
 	private char[] operands;
@@ -29,9 +34,16 @@ public class GameBoardController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		RandomizeGameboard ();
+
+		//These are just tests for now until we have a ui
+		//We might also consider making an array of answers
+		//Not sure yet but they work is the good news
 		GenerateRowAnswer ((int) row.FirstRow);
 		GenerateRowAnswer ((int) row.SecondRow);
 		GenerateRowAnswer ((int) row.ThirdRow);
+		GenerateColumnAnswer ((int) column.FirstColumn);
+		GenerateColumnAnswer ((int) column.SecondColumn);
+		GenerateColumnAnswer ((int) column.ThirdColumn);
 	}
 	
 	// Update is called once per frame
@@ -87,6 +99,7 @@ public class GameBoardController : MonoBehaviour {
 	/// This currently does not work if we change row count
 	/// </summary>
 	/// <returns>The row answer.</returns>
+	/// <param name="RowIndex">The row to calculate</param> 
 	public double GenerateRowAnswer(int RowIndex) {
 		double answer;
 
@@ -105,6 +118,34 @@ public class GameBoardController : MonoBehaviour {
 		Expression expression = new Expression (expressionString);
 
 		answer = System.Convert.ToDouble(expression.Evaluate ());
+
+		return answer;
+	}
+
+	/// <summary>
+	/// Generates the answer for a single column
+	/// This does not work for if we change the column count
+	/// </summary>
+	/// <returns>The column answer.</returns>
+	/// <param name="ColumnIndex">The column to calculate</param>
+	public double GenerateColumnAnswer(int ColumnIndex) {
+		double answer;
+
+		//Locate which operands to use based on the column
+		int operandsStartingIndex = ColumnIndex + 2; //todo: write this
+
+		//Generates the expression string for the column
+		string expressionString = string.Format ("{0}{1}{2}{3}{4}",
+			                          gameboardNums [0, ColumnIndex],
+			                          operands [operandsStartingIndex],
+			                          gameboardNums [1, ColumnIndex],
+			                          operands [operandsStartingIndex + GAMEBOARD_WIDTH],
+			                          gameboardNums [2, ColumnIndex]	
+		                          );
+
+		Expression expression = new Expression (expressionString);
+
+		answer = System.Convert.ToDouble (expression.Evaluate ());
 
 		return answer;
 	}
